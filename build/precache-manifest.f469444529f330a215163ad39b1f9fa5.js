1d2a92387c491a0b0903bc393c1533b1a022533f
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2020-12-07 21:45:45 -0600

    dot env

---
+++ build/precache-manifest.f469444529f330a215163ad39b1f9fa5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577286e0ad0fd4936b12e69552bec703",
+    "url": "/index.html"
+  },
+  {
+    "revision": "814e314b4aef3f3db9ee",
+    "url": "/static/css/main.2740a7f0.chunk.css"
+  },
+  {
+    "revision": "981a13d086e8178b11cd",
+    "url": "/static/js/2.2d7974a0.chunk.js"
+  },
+  {
+    "revision": "88c713d06b20c302223622c54545b981",
+    "url": "/static/js/2.2d7974a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "814e314b4aef3f3db9ee",
+    "url": "/static/js/main.3d185484.chunk.js"
+  },
+  {
+    "revision": "97761f348a4d0f42c7b9",
+    "url": "/static/js/runtime-main.2a5d10e1.js"
+  },
+  {
+    "revision": "f8a2771f0ffe8890e34892592d619e51",
+    "url": "/static/media/logo-brown.f8a2771f.png"
+  },
+  {
+    "revision": "48118b2950a6f3c8283747eb64d2e878",
+    "url": "/static/media/placeholder-beer.48118b29.png"
+  }
+]);
